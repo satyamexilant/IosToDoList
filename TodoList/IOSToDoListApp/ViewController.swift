c1f081f7c6f51306a636ca0b5e7ff1154513098c
medidi vv satyanarayana murty <medidi.murty@exilant.com>
2017-08-01 12:07:45 +0530

    Project SetUp

---
+++ TodoList/IOSToDoListApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOSToDoListApp
+//
+//  Created by medidi vv satyanarayana murty on 01/08/17.
+//  Copyright © 2017 medidi vv satyanarayana murty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
